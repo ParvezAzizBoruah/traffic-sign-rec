97d013d2534939ff9da40782f0d345d963760f9f
ParvezAzizBoruah <35813099+ParvezAzizBoruah@users.noreply.github.com>
2018-02-23 12:45:46 +0530

    Update ht.py

--- ht.py
+++ ht.py
@@ -1,5 +1,5 @@
 import cv2
 from matplotlib import pyplot as plt
-img = cv2.imread('ff.jpg',0)
-plt.hist(img.ravel(),256,[0,256])
+img = cv2.imread('ff.jpg',0)#read image
+plt.hist(img.ravel(),256,[0,256])#plot histogram
 plt.show()
